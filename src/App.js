18c77059cfefbcf1f58830ae980d9217c0f7d767
quyetpham <quyetpham>
2022-12-14 11:33:54 +0700

    update main

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Formdev from './components/formdev';
-import { emitter } from './components/emitter';
 
 function App({}) {
-  useEffect(() => {
-    emitter.on("test2", (data) => {
-      fireCha(data);
-    }, [])
-  }, []);
-  const fireCha = () => {
-    emitter.emit("test", "data011111111111111")
-  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
-          <button
-            onClick={() => fireCha()}
-          >
-            onclick Con
-          </button>
-        </div>
-        <div>
-          <Formdev/>
+          <h1>branch Main</h1>
         </div>
       </header>
     </div>
